e0a3f8039c3bfe73f446439e11465bc6fba3d1d5
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2018-10-19 00:59:09 -0400

    Reformat TimerManager class

--- src/main/java/io/github/vhoyon/vramework/utilities/TimerManager.java
+++ src/main/java/io/github/vhoyon/vramework/utilities/TimerManager.java
@@ -5,45 +5,48 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public final class TimerManager {
-
-	private static HashMap<String,Timer> timers;
-
+	
+	private static HashMap<String, Timer> timers;
+	
 	private TimerManager(){}
-
+	
 	/**
-	 * @param timerName Name of the timer
-	 * @param delay Delay (in milliseconds) before doing the action
-	 * @param action Action to do after the delay was finished, without being called again, which resets the delay
+	 * @param timerName
+	 *            Name of the timer
+	 * @param delay
+	 *            Delay (in milliseconds) before doing the action
+	 * @param action
+	 *            Action to do after the delay was finished, without being
+	 *            called again, which resets the delay
 	 */
-	public static void schedule(final String timerName, final int delay, Runnable action){
-
+	public static void schedule(final String timerName, final int delay,
+			Runnable action){
+		
 		if(timers == null)
 			timers = new HashMap<>();
 		
-		TimerTask task = new TimerTask() {
+		TimerTask task = new TimerTask(){
 			@Override
-			public void run() {
+			public void run(){
 				action.run();
 			}
-		} ;
+		};
 		
 		stopTimer(timerName);
 		
 		Timer timer = new Timer(timerName);
 		
 		timer.schedule(task, delay);
-
-
-		timers.put(timerName,timer);
 		
-
+		timers.put(timerName, timer);
+		
 	}
-
+	
 	public static void stopTimer(String timerName){
-
+		
 		if(timers != null && timers.containsKey(timerName))
 			timers.remove(timerName).cancel();
-
+		
 	}
-
+	
 }
