0ff9c1cc0ea4dd23bbe4ce29bf8a5b9d13fb4a84
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2018-12-30 14:53:28 -0500

    Add object keys creation in KeyBuilder utility A new method was added for all existing methods to allow for an easier and consistent key creation for identifiers (the Buffer object, for example).

--- src/main/java/io/github/vhoyon/vramework/utilities/KeyBuilder.java
+++ src/main/java/io/github/vhoyon/vramework/utilities/KeyBuilder.java
@@ -15,26 +15,54 @@ public class KeyBuilder {
 		return Utils.buildKey(guild.getId());
 	}
 	
+	public static String buildGuildObjectKey(Guild guild, Object object){
+		return Utils.buildKey(buildGuildKey(guild), object.toString());
+	}
+	
 	public static String buildTextChannelKey(TextChannel channel){
 		return Utils.buildKey(buildGuildKey(channel.getGuild()),
 				channel.getId());
+	}
+	
+	public static String buildTextChannelObjectKey(TextChannel channel,
+			Object object){
+		return Utils.buildKey(buildTextChannelKey(channel), object.toString());
 	}
 	
 	public static String buildUserKey(User user){
 		return Utils.buildKey(user.getName(), user.getId());
 	}
 	
+	public static String buildUserObjectKey(User user, Object object){
+		return Utils.buildKey(buildUserKey(user), object.toString());
+	}
+	
 	public static String buildUserKey(Member member){
 		return buildUserKey(member.getUser());
+	}
+	
+	public static String buildUserObjectKey(Member member, Object object){
+		return buildUserObjectKey(member.getUser(), object);
 	}
 	
 	public static String buildCommandKey(TextChannel channel, String commandName){
 		return Utils.buildKey(buildTextChannelKey(channel), commandName);
 	}
 	
+	public static String buildCommandObjectKey(TextChannel channel,
+			String commandName, Object object){
+		return Utils.buildKey(buildCommandKey(channel, commandName),
+				object.toString());
+	}
+	
 	public static String buildCommandKey(AbstractBotCommand command){
 		return buildCommandKey(command.getTextContext(),
 				command.getDefaultCall());
+	}
+	
+	public static String buildCOmmandObjectKey(AbstractBotCommand command,
+			Object object){
+		return Utils.buildKey(buildCommandKey(command), object.toString());
 	}
 	
 }
