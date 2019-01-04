ea1e5cf5d9dfc61f48d848060ae90adc3215964c
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-01-03 21:57:14 -0500

    Add methods to easily deal with Singleton classes in the Buffer
    The new methods allows the creation of the Singleton Design Pattern for classes to use the Buffer as a centralized object for easier portability and access through the whole application.

--- src/main/java/io/github/vhoyon/vramework/objects/Buffer.java
+++ src/main/java/io/github/vhoyon/vramework/objects/Buffer.java
@@ -1,6 +1,7 @@
 package io.github.vhoyon.vramework.objects;
 
 import java.util.HashMap;
+import java.util.concurrent.Callable;
 
 import io.github.vhoyon.vramework.interfaces.Utils;
 
@@ -19,6 +20,55 @@ public class Buffer {
 			buffer = new Buffer();
 		
 		return buffer;
+	}
+	
+	public static <E> E getSingleton(Class<E> desiredClass,
+			Callable<E> createInstanceIfNew)
+			throws UnsupportedOperationException{
+		
+		if(desiredClass == null){
+			throw new IllegalArgumentException(
+					"The desiredClass parameter cannot be null!");
+		}
+		
+		String classKey = createSingletonClassKey(desiredClass);
+		
+		Buffer buffer = Buffer.get();
+		
+		if(buffer.has(classKey)){
+			return (E)buffer.get(classKey);
+		}
+		else{
+			
+			E newClassSingleton = null;
+			
+			try{
+				newClassSingleton = createInstanceIfNew.call();
+			}
+			catch(Exception e){
+				throw new UnsupportedOperationException(
+						"The code provided to create the new singleton instance threw an Exception.",
+						e);
+			}
+			
+			buffer.push(newClassSingleton, classKey);
+			
+			return newClassSingleton;
+			
+		}
+		
+	}
+	
+	public static boolean removeSingleton(Class<?> singletonClass){
+		return Buffer.get().remove(createSingletonClassKey(singletonClass));
+	}
+	
+	public static boolean hasSingleton(Class<?> singletonClass){
+		return Buffer.get().has(createSingletonClassKey(singletonClass));
+	}
+	
+	private static String createSingletonClassKey(Class<?> singletonClass){
+		return Utils.buildKey("SINGLETON", singletonClass.getCanonicalName());
 	}
 	
 	public boolean push(Object object, String associatedName, String key){
